bdd062c15a18d33b7abad31a3ccafd0d8410f7b1
Rutam Rajhansa <rsrajhansa@gmail.com>
2024-06-18 19:57:24 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function getRecommendations() {
+    const mood = document.getElementById('mood').value;
+
+    fetch(`/recommendations?mood=${mood}`)
+    .then(response => response.json())
+    .then(data => {
+        displayRecommendations(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+function displayRecommendations(data) {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    if (data.success) {
+        const recommendations = data.recommendations;
+        const resultHTML = recommendations.map(rec => `
+            <div>
+                <h3>${rec.title}</h3>
+                <p>${rec.artist}</p>
+                <iframe src="${rec.embedUrl}" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            </div>
+        `).join('');
+        resultDiv.innerHTML = resultHTML;
+    } else {
+        resultDiv.innerHTML = '<p>No recommendations found.</p>';
+    }
+}
